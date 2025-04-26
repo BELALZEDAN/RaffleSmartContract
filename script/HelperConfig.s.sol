f3a05f6dfc942f32acb02d907a6c9faa1563f94c
BELALZEDAN <belalzedan72@gmail.com>
2025-04-26 11:05:59 +0200

    make Raffle

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 subscriptionId;
+        bytes32 gasLane;
+        uint256 interval;
+        uint256 entranceFee;
+        uint32 callbackGasLimit;
+        address vrfCoordinatorV2;
+    }
+}
